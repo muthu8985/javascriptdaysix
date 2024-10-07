d0cb08b5446bd2c0dca223e8231813271e5f65f1
Muthu Kumar <muthukumarv8985@gmail.com>
2024-10-07 14:09:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let template=`hello world!`
+document.write(template);
+// inside the  template string in quotes
+let temp1=`"hello my world"`
+document.write(temp1);
+// variable subsititutions
+let firstName = "John";
+let lastName = "Doe";
+
+let text = `Welcome ${firstName}, ${lastName}!`;
